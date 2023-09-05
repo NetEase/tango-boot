7cc3dcb4f30a9e7972e2dff608a1873a239b7977
wwsun <ww.sww@outlook.com>
2023-09-05 17:39:39 +0800

    fix: add pathVariables support

--- packages/request/src/request.ts
+++ packages/request/src/request.ts
@@ -2,7 +2,16 @@ import axios from 'axios';
 import type { AxiosRequestConfig } from 'axios';
 
 export interface RequestConfig extends AxiosRequestConfig {
+  /**
+   * 响应格式化函数
+   * @param data 响应数据
+   * @returns 返回格式化后的数据
+   */
   formatter?: (data: any) => any;
+  /**
+   * 路径变量，例如 `/users/:id`，其中 id 为路径变量
+   */
+  pathVariables?: Record<string, any>;
 }
 
 function createInstance(instanceConfig?: AxiosRequestConfig) {
@@ -10,14 +19,18 @@ function createInstance(instanceConfig?: AxiosRequestConfig) {
   const instance = axios.create(instanceConfig);
 
   // request 拦截器
-  // instance.interceptors.request.use(
-  //   (config) => {
-  //     return config;
-  //   },
-  //   (error) => {
-  //     return Promise.reject(error);
-  //   }
-  // );
+  instance.interceptors.request.use(
+    (config) => {
+      const { pathVariables } = config as RequestConfig;
+      if (pathVariables) {
+        config.url = config.url.replace(/\/:(\w+)/gi, (_, key) => `/${pathVariables[key]}`);
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
 
   // response 拦截器
   instance.interceptors.response.use(
